6eac3182e62dbec102892467db6696aacd9c9c57
Taron Afrikyan <afrikyantaron@mail.ru>
2026-01-31 19:39:57 +0300

    Домашнее задание 02

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const title = '';
-const screens = '';
-let screenPrice = 0;
+const title = 'Проект интенсива Glo Academy';
+const screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 50;
 const rollback = 10;
-let fullPrice = 0;
+let fullPrice = 100;
 const adaptive = true;
 
 alert('Предупреждение с любым текстом');
-console.log('Сообщение в консоли с любым текстом')
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов:', screenPrice, 'рублей', 'и Стоимость разработки сайта:', fullPrice, 'рублей');
+console.log(screens.toLowerCase().split(', '));
+console.log('Процент отката посреднику за работу', (fullPrice * (rollback/100)));
