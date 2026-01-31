fea845cf5808477cf8bb028e348cf62645b9f3df
Taron Afrikyan <afrikyantaron@mail.ru>
2026-01-31 19:08:48 +0300

    Домашнее задание 01

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+const title = '';
+const screens = '';
+let screenPrice = 0;
+const rollback = 10;
+let fullPrice = 0;
+const adaptive = true;
+
 alert('Предупреждение с любым текстом');
 console.log('Сообщение в консоли с любым текстом')
