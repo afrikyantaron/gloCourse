721addd14fae27767344d2a9e62a69aa0f4c5f56
Taron Afrikyan <afrikyantaron@mail.ru>
2026-02-07 21:28:17 +0300

    Домашнее задание 03

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const title = 'Проект интенсива Glo Academy';
-const screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 50;
-const rollback = 10;
-let fullPrice = 100;
-const adaptive = true;
+'use strict'
 
-console.log(title, fullPrice, adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов:', screenPrice, 'рублей', 'и Стоимость разработки сайта:', fullPrice, 'рублей');
-console.log(screens.toLowerCase().split(', '));
-console.log('Процент отката посреднику за работу', (fullPrice * (rollback/100)));
+const title = prompt("Как называется Ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Пример: " + "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "Пример: " + "12000");
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+
+const service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+
+let rollback = 10;
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice * (1 - rollback / 100));
+
+console.log("Итоговая стоимость за вычетом отката посреднику: " + servicePercentPrice);
+
+if (fullPrice <= 0) {
+    console.log("Что-то пошло не так");
+} else if (fullPrice > 30000) {
+    console.log("Даем скидку в 10%");
+} else if (fullPrice >= 15000) {
+    console.log("Даем скидку в 5%");
+} else {
+    console.log("Скидка не предусмотрена");
+}
